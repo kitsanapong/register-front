37f19505c64c4e57613ebd8adf03ce6522d8eed7
Kitsanapong Roubgome <kitsanapong.r@gmail.com>
2018-11-17 14:38:55 +0700

    Create GuestLanding component

---
+++ app/components/GuestLanding/index.js
@@ -0,0 +1,20 @@
+/**
+ *
+ * GuestLanding
+ *
+ */
+
+import React from 'react';
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+/* eslint-disable react/prefer-stateless-function */
+class GuestLanding extends React.Component {
+  render() {
+    return <div />;
+  }
+}
+
+GuestLanding.propTypes = {};
+
+export default GuestLanding;
