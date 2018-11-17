8eae7aaf625f8eda08e39aef30925f2bbf76fac5
Kitsanapong Roubgome <kitsanapong.r@gmail.com>
2018-11-17 14:26:59 +0700

    Create LoginPage

---
+++ app/containers/LoginPage/index.js
@@ -0,0 +1,52 @@
+/**
+ *
+ * LoginPage
+ *
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { compose } from 'redux';
+
+import injectSaga from 'utils/injectSaga';
+import injectReducer from 'utils/injectReducer';
+import makeSelectLoginPage from './selectors';
+import reducer from './reducer';
+import saga from './saga';
+
+/* eslint-disable react/prefer-stateless-function */
+export class LoginPage extends React.Component {
+  render() {
+    return <div />;
+  }
+}
+
+LoginPage.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = createStructuredSelector({
+  loginPage: makeSelectLoginPage(),
+});
+
+function mapDispatchToProps(dispatch) {
+  return {
+    dispatch,
+  };
+}
+
+const withConnect = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+);
+
+const withReducer = injectReducer({ key: 'loginPage', reducer });
+const withSaga = injectSaga({ key: 'loginPage', saga });
+
+export default compose(
+  withReducer,
+  withSaga,
+  withConnect,
+)(LoginPage);
