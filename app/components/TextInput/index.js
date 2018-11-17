e672f2653710697102c62cf168ac9fad739777a2
Kitsanapong Roubgome <kitsanapong.r@gmail.com>
2018-11-18 02:58:31 +0700

    Create TextInput component

---
+++ app/components/TextInput/index.js
@@ -0,0 +1,20 @@
+/**
+ *
+ * TextInput
+ *
+ */
+
+import React from 'react';
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+/* eslint-disable react/prefer-stateless-function */
+class TextInput extends React.Component {
+  render() {
+    return <div />;
+  }
+}
+
+TextInput.propTypes = {};
+
+export default TextInput;
---
+++ app/components/TextInput/tests/index.test.js
@@ -0,0 +1,11 @@
+// import React from 'react';
+// import { mount } from 'enzyme';
+// import { enzymeFind } from 'styled-components/test-utils';
+
+// import TextInput from '../index';
+
+describe('<TextInput />', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
