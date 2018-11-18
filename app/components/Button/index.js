151ed26531607dff6fe6e965e797ca7f6b54b722
Kitsanapong Roubgome <kitsanapong.r@gmail.com>
2018-11-18 21:50:50 +0700

    Create Button component

---
+++ app/components/Button/index.js
@@ -0,0 +1,20 @@
+/**
+ *
+ * Button
+ *
+ */
+
+import React from 'react';
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+/* eslint-disable react/prefer-stateless-function */
+class Button extends React.Component {
+  render() {
+    return <div />;
+  }
+}
+
+Button.propTypes = {};
+
+export default Button;
