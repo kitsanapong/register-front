8066e1d8aa772556e0710bb7468fd34aec64b38c
Kitsanapong Roubgome <kitsanapong.r@gmail.com>
2018-11-19 15:22:09 +0700

    Improve backdrop: change image and desciption overtime with loop

--- app/components/GuestLanding/index.js
+++ app/components/GuestLanding/index.js
@@ -9,6 +9,44 @@ import React from 'react';
 import styled from 'styled-components';
 
 import GamersImage from 'images/gamers.jpg'
+import PublishersImage from 'images/publishers.jpg'
+import OrganizersImage from 'images/11.jpg'
+import TeamsImage from 'images/teams.jpg'
+import SponsorsImage from 'images/sponsors1.jpg'
+import InvestorsImage from 'images/investors.jpg'
+
+const BACKDROP_DETAILS = [
+  {
+    image: GamersImage,
+    title: 'GAMERS',
+    detail: 'Gamers on different levels across different games will be able to compete and make money on Gamico.',
+  },
+  {
+    image: PublishersImage,
+    title: 'PUBLISHERS',
+    detail: 'Game publishers will be able to integrate their games to Gamico and thus create automatic head-to-head battles.',
+  },
+  {
+    image: OrganizersImage,
+    title: 'ORGANIZERS',
+    detail: 'Gamico is the one stop location where esports tournament organizers can host online tournaments.',
+  },
+  {
+    image: TeamsImage,
+    title: 'TEAMS',
+    detail: 'Esports teams can not only compete on Gamico but also use it to scout new players and find sponsors and investors.',
+  },
+  {
+    image: SponsorsImage,
+    title: 'SPONSORS',
+    detail: 'Companies will have a chance to sponsor esports tournaments and in doing so increase the reach of their brand.',
+  },
+  {
+    image: InvestorsImage,
+    title: 'INVESTORS',
+    detail: 'Investors will have a chance to not only invest in esports teams but sponsor individual esports players as well.',
+  },
+]
 
 const StyledComponents = styled.div`
   width: 720px;
@@ -58,9 +96,43 @@ const StyledComponents = styled.div`
 
 /* eslint-disable react/prefer-stateless-function */
 class GuestLanding extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      backdropIndex: 0,
+    }
+  }
+
+  componentDidMount() {
+    this.rotateBackdropImage()
+  }
+
   renderForm() {
     const { children } = this.props
     return children
+  }
+
+  rotateBackdropImage() {
+    setInterval(() => {
+      this.setState({
+        backdropIndex: this.state.backdropIndex + 1,
+      })
+    }, 5000)
+  }
+
+  rednerBackdropImage() {
+    const index = this.state.backdropIndex % BACKDROP_DETAILS.length
+    return BACKDROP_DETAILS[index].image
+  }
+
+  renderBackdropTitle() {
+    const index = this.state.backdropIndex % BACKDROP_DETAILS.length
+    return BACKDROP_DETAILS[index].title
+  }
+
+  renderBackdropDetail() {
+    const index = this.state.backdropIndex % BACKDROP_DETAILS.length
+    return BACKDROP_DETAILS[index].detail
   }
 
   render() {
@@ -68,12 +140,12 @@ class GuestLanding extends React.Component {
       <StyledComponents>
         <div className="left">
           <div className="backdrop-wrapper">
-            <img src={GamersImage} />
+            <img src={this.rednerBackdropImage()} />
           </div>
           <div className="description-wrapper">
-            <div className="title">GAMERS</div>
+            <div className="title">{ this.renderBackdropTitle() }</div>
             <div className="divider"></div>
-            <div className="detail">Gamers on different levels across different games will be able to compete and make money on Gamico.</div>
+            <div className="detail">{ this.renderBackdropDetail() }</div>
           </div>
         </div>
         <div className="right">
