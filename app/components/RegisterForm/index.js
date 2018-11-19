b808efdcef4f4a541ad3d55a6b25d6e80f27b594
Kitsanapong Roubgome <kitsanapong.r@gmail.com>
2018-11-19 16:44:50 +0700

    Create RegisterForm component

---
+++ app/components/RegisterForm/index.js
@@ -0,0 +1,20 @@
+/**
+ *
+ * RegisterForm
+ *
+ */
+
+import React from 'react';
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+/* eslint-disable react/prefer-stateless-function */
+class RegisterForm extends React.Component {
+  render() {
+    return <div />;
+  }
+}
+
+RegisterForm.propTypes = {};
+
+export default RegisterForm;
---
+++ app/components/RegisterForm/tests/index.test.js
@@ -0,0 +1,11 @@
+// import React from 'react';
+// import { mount } from 'enzyme';
+// import { enzymeFind } from 'styled-components/test-utils';
+
+// import RegisterForm from '../index';
+
+describe('<RegisterForm />', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
