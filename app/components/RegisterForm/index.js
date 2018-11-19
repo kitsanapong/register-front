4ed74e117a2095b2a626b364fc79a3fe9d7204d9
Kitsanapong Roubgome <kitsanapong.r@gmail.com>
2018-11-19 17:08:42 +0700

    Copy LoginForm to RegisterForm

--- app/components/RegisterForm/index.js
+++ app/components/RegisterForm/index.js
@@ -6,12 +6,72 @@
 
 import React from 'react';
 // import PropTypes from 'prop-types';
-// import styled from 'styled-components';
+import styled from 'styled-components';
+import { MdEmail, MdVpnKey, MdPerson } from 'react-icons/md'
+
+import TextInput from 'components/TextInput'
+import Button from 'components/Button'
+
+const StyledCompoent = styled.div`
+  padding: 48px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100%;
+  .register-wrapper {
+    text-align: center;
+    .text {
+      font-size: 14px;
+      opacity: 0.54;
+    }
+    .highlight {
+      margin-left: 6px;
+      cursor: pointer;
+      font-size: 14px;
+      color: rgb(24,21,37);
+      opacity: 1;
+      font-weight: bold;
+    }
+  }
+`
 
 /* eslint-disable react/prefer-stateless-function */
 class RegisterForm extends React.Component {
   render() {
-    return <div />;
+    return (
+      <StyledCompoent>
+        <div className="header">
+        </div>
+        <div className="input-wrapper">
+          <TextInput
+            id="username"
+            active={true}
+            icon={<MdPerson />}
+            placeholder="Username"
+            type="text"
+          />
+          <TextInput
+            id="email"
+            icon={<MdEmail />}
+            placeholder="Email"
+            type="text"
+          />
+          <TextInput
+            id="password"
+            icon={<MdVpnKey />}
+            placeholder="Password"
+            type="password"
+          />
+        </div>
+        <div className="button-wrapper">
+          <Button id="submit-button" label="LOGIN" />
+        </div>
+        <div className="register-wrapper">
+          <span className="text">Don't have an account?</span>
+          <span className="highlight">Sign up</span>
+        </div>
+      </StyledCompoent>
+    )
   }
 }
 
--- app/components/RegisterForm/tests/index.test.js
+++ app/components/RegisterForm/tests/index.test.js
@@ -1,11 +1,32 @@
-// import React from 'react';
-// import { mount } from 'enzyme';
+import React from 'react';
+import { shallow } from 'enzyme';
 // import { enzymeFind } from 'styled-components/test-utils';
 
-// import RegisterForm from '../index';
+import RegisterForm from '../index';
 
 describe('<RegisterForm />', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(false);
-  });
+  it('have Username', () => {
+    const wrapper = shallow(
+      <RegisterForm />
+    )
+    expect(wrapper.find('#username').length).toEqual(1)
+  })
+  it('have Email', () => {
+    const wrapper = shallow(
+      <RegisterForm />
+    )
+    expect(wrapper.find('#email').length).toEqual(1)
+  })
+  it('have Password', () => {
+    const wrapper = shallow(
+      <RegisterForm />
+    )
+    expect(wrapper.find('#password').length).toEqual(1)
+  })
+  it('have Submit Button', () => {
+    const wrapper = shallow(
+      <RegisterForm />
+    )
+    expect(wrapper.find('#submit-button').length).toEqual(1)
+  })
 });
