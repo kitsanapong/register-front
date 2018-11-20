837a0bc81b8653d6cf4f49eb4f9452817cbc0329
Kitsanapong Roubgome <kitsanapong.r@gmail.com>
2018-11-20 18:14:52 +0700

    Login consts

--- app/containers/LoginPage/constants.js
+++ app/containers/LoginPage/constants.js
@@ -10,3 +10,8 @@ export const REGISTER = 'app/LoginPage/REGISTER';
 export const REGISTER_REQUEST = 'app/LoginPage/REGISTER_REQUEST';
 export const REGISTER_SUCCEED = 'app/LoginPage/REGISTER_SUCCEED';
 export const REGISTER_FAILED = 'app/LoginPage/REGISTER_FAILED';
+
+export const LOGIN = 'app/LoginPage/LOGIN';
+export const LOGIN_REQUEST = 'app/LoginPage/LOGIN_REQUEST';
+export const LOGIN_SUCCEED = 'app/LoginPage/LOGIN_SUCCEED';
+export const LOGIN_FAILED = 'app/LoginPage/LOGIN_FAILED';
