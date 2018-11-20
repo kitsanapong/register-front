3fe2a6833cd8a8b8e191bfa327b83f85c50cd49c
Kitsanapong Roubgome <kitsanapong.r@gmail.com>
2018-11-20 16:14:15 +0700

    Register Saga

--- app/containers/LoginPage/saga.js
+++ app/containers/LoginPage/saga.js
@@ -1,6 +1,24 @@
-// import { take, call, put, select } from 'redux-saga/effects';
+import { takeLatest, call, put, select } from 'redux-saga/effects';
+
+import request, { POSTOption } from 'utils/request'
+import { REGISTER_URL } from 'utils/apis'
+
+import { Register, RegisterRequest, RegisterSucceed, RegisterFailed } from './actions'
+
+export function* RegisterSaga(action) {
+  yield put(RegisterRequest())
+  try {
+    const url = REGISTER_URL
+    const option = POSTOption(action.payload)
+    yield call(request, url, option)
+    yield put(RegisterSucceed())
+  }
+  catch (err) {
+    yield put(RegisterFailed({ err }))
+  }
+}
 
 // Individual exports for testing
 export default function* loginPageSaga() {
-  // See example in containers/HomePage/saga.js
+  yield takeLatest(Register().type, RegisterSaga)
 }
--- app/containers/LoginPage/tests/saga.test.js
+++ app/containers/LoginPage/tests/saga.test.js
@@ -3,13 +3,49 @@
  */
 
 /* eslint-disable redux-saga/yield-effects */
-// import { take, call, put, select } from 'redux-saga/effects';
-// import loginPageSaga from '../saga';
+import { take, call, put, select } from 'redux-saga/effects';
 
-// const generator = loginPageSaga();
+import request, { POSTOption } from 'utils/request'
+import { REGISTER_URL } from 'utils/apis'
+
+import loginPageSaga, { RegisterSaga } from '../saga';
+import {
+  Register,
+  RegisterRequest,
+  RegisterSucceed,
+  RegisterFailed,
+} from '../actions'
+
+const generator = loginPageSaga();
 
 describe('loginPageSaga Saga', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(false);
-  });
+  describe('RegisterSaga', () => {
+    it('should be defined', () => {
+      expect(RegisterSaga).toBeDefined()
+    })
+    describe('Call', () => {
+      let saga
+      const action = Register({
+        username: 'user',
+        email: 'email',
+        password: 'password',
+      })
+      saga = RegisterSaga(action)
+      it('should put RegisterRequest', () => {
+        expect(saga.next().value).toEqual(put(RegisterRequest()))
+      })
+      it('should call register api', () => {
+        const url = REGISTER_URL
+        const option = POSTOption(action.payload)
+        expect(saga.next().value).toEqual(call(request, url, option))
+      })
+      it('should put RegisterSucceed', () => {
+        expect(saga.next().value).toEqual(put(RegisterSucceed()))
+      })
+      it('should put RegisterFailed', () => {
+        const err = {}
+        expect(saga.throw(err).value).toEqual(put(RegisterFailed({ err })))
+      })
+    })
+  })
 });
