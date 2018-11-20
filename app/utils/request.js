eee7a49d1b25721942b0aab25ff2fb6425a34e6c
Kitsanapong Roubgome <kitsanapong.r@gmail.com>
2018-11-20 15:49:42 +0700

    Request for api call

---
+++ app/utils/request.js
@@ -0,0 +1,17 @@
+import { fetch } from 'whatwg-fetch'
+
+export const POSTOption = (data) => {
+  return {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      ...data,  
+    })
+  }
+}
+
+export default function request({ url, option }) {
+  return fetch(url, option)
+}
