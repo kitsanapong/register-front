b2cf7353c365cc717ec64728c47dafd13f1168c1
Kitsanapong Roubgome <kitsanapong.r@gmail.com>
2018-11-22 01:33:16 +0700

    Change api urls

--- app/utils/apis.js
+++ app/utils/apis.js
@@ -1,3 +1,5 @@
-export const REGISTER_URL = 'http://localhost:3001/user/'
-export const LOGIN_URL = 'http://localhost:3001/user/login'
+export const REGISTER_URL = 'http://localhost:3001/public/'
+export const LOGIN_URL = 'http://localhost:3001/public/login'
+
+export const GET_USER_URL = 'http://localhost:3001/user/'
 export const LOGOUT_URL = 'http://localhost:3001/user/logout'
