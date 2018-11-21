c4ac158f178f94537ecebb6b3247012a9eeb188e
Kitsanapong Roubgome <kitsanapong.r@gmail.com>
2018-11-21 16:43:39 +0700

    Fix urls

--- app/utils/apis.js
+++ app/utils/apis.js
@@ -1,3 +1,3 @@
-export const REGISTER_URL = 'XXXXXXXX'
-export const LOGIN_URL = 'YYYYYYYY'
-export const LOGOUT_URL = 'ZZZZZZZZZ'
+export const REGISTER_URL = 'http://localhost:3001/user/'
+export const LOGIN_URL = 'http://localhost:3001/user/login'
+export const LOGOUT_URL = 'http://localhost:3001/user/logout'
